d784881236a03484d82c0cfb08be31d12cb438d1
SODABYTES <146698058+SODABYTES@users.noreply.github.com>
2026-08-30 14:10:18 -0400

    Scoring from pyramid value no longer exponential

--- src/PyramisTriangle.c
+++ src/PyramisTriangle.c
@@ -11,6 +11,10 @@ void PyramisTriangleSetDefaultVariables(PyramisTriangle* triangle)
 
 unsigned int PyramisTriangleGetScoreFromPyramidValue(PyramisTriangle triangle)
 {
+    return triangle.pyramidValue * 300;
+
+    // OLDER EXPONENTIAL LOGIC THAT I'M KEEPING COMMENTED OUT JUST IN CASE?
+    /*
     unsigned int result = 1;
     for (unsigned int i = 0; i < triangle.pyramidValue - 1; i++)
     {
@@ -18,4 +22,5 @@ unsigned int PyramisTriangleGetScoreFromPyramidValue(PyramisTriangle triangle)
     }
     result = result * 300;
     return result;
+    */
 }
